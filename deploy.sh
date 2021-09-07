0c441794d00a9b915e8cb2652cae8fa2143729fa
Matthew Mattox <matt.mattox@suse.com>
2021-09-07 01:23:18 -0500

    fixing master ingress

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 echo "customizing Deployment files..."
-mkdir ./deployment-ready/
-cd ./deployment
+mkdir /drone/src/k8s/deployment-ready/
+cd /drone/src/k8s/deployment-ready/
 for file in `ls *.yaml`
 do
   echo "Working on $file"
-  cat $file | sed "s/BUILD_NUMBER/${CI_BUILD_NUMBER}/g" > ../deployment-ready/"$file"
+  cat $file | sed "s/BUILD_NUMBER/${CI_BUILD_NUMBER}/g" > /drone/src/k8s/deployment-ready/"$file"
 done
+
+ls -l /drone/src/k8s/deployment-ready/
+cat /drone/src/k8s/ingress/master.yaml | sed "s/BUILD_NUMBER/${CI_BUILD_NUMBER}/g" > /drone/src/k8s/ingress/master.yaml-tmp
+mv /drone/src/k8s/ingress/master.yaml-tmp /drone/src/k8s/ingress/master.yaml
