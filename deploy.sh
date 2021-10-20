d4ec575170158f548fbcfdb4d237859490559282
Matthew Mattox <mmattox@support.tools>
2021-10-19 19:58:37 -0500

    Removing master ingress

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,3 @@ do
   echo "Working on $file"
   cat $file | sed "s/BUILD_NUMBER/${CI_BUILD_NUMBER}/g" > /drone/src/deployment-ready/"$file"
 done
-
-ls -l /drone/src/deployment-ready/
-cat /drone/src/deployment/ingress/master.yaml | sed "s/BUILD_NUMBER/${CI_BUILD_NUMBER}/g" > /drone/src/deployment/ingress/master.yaml-tmp
-mv /drone/src/deployment/ingress/master.yaml-tmp /drone/src/deployment/ingress/master.yaml
