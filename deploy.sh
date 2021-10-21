2b32102d3996107dc07768f564fe0b9e817289d6
Matthew Mattox <mmattox@support.tools>
2021-10-21 01:58:13 -0500

    Switching docker build only on master

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+if [[ -z $BUILD_NUMBER ]]
+then
+  BUILD_NUMBER=${CI_BUILD_NUMBER}
+fi
+
 echo "customizing Deployment files..."
 mkdir /drone/src/deployment-ready/
 cd /drone/src/deployment/
 for file in `ls *.yaml`
 do
   echo "Working on $file"
-  cat $file | sed "s/BUILD_NUMBER/${CI_BUILD_NUMBER}/g" > /drone/src/deployment-ready/"$file"
+  cat $file | sed "s/BUILD_NUMBER/${BUILD_NUMBER}/g" > /drone/src/deployment-ready/"$file"
 done
-
-ls -l /drone/src/deployment-ready/
-cat /drone/src/deployment/ingress/master.yaml | sed "s/BUILD_NUMBER/${CI_BUILD_NUMBER}/g" > /drone/src/deployment/ingress/master.yaml-tmp
-mv /drone/src/deployment/ingress/master.yaml-tmp /drone/src/deployment/ingress/master.yaml
