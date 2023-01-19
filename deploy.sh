35669d560bfaf5067c7f23e33b12950309fccdcb
Matthew Mattox <mmattox@support.tools>
2023-01-19 16:51:31 -0600

    Cleaning up

--- deploy.sh
+++ deploy.sh
@@ -127,14 +127,15 @@ helm package ./chart/ --version ${DRONE_BUILD_NUMBER} --app-version ${imagetag}
 helm push website-helm-${DRONE_BUILD_NUMBER}.tgz oci://harbor.support.tools/supporttools
 rm -f website-helm-${DRONE_BUILD_NUMBER}.tgz
 
-echo "Waiting for pods to become ready..."
-echo "Checking Deployments"
-for deployment in `kubectl -n ${namespace} get deployment -o name`
-do
-  echo "Checking ${deployment}"
-  kubectl -n ${namespace} rollout status ${deployment}
-done
+# echo "Waiting for pods to become ready..."
+# echo "Checking Deployments"
+# for deployment in `kubectl -n ${namespace} get deployment -o name`
+# do
+#   echo "Checking ${deployment}"
+#   kubectl -n ${namespace} rollout status ${deployment}
+# done
 
+echo "Syncing files to CDN..."
 if [ ${synccdn} == true ];
 then
   echo "Syncing files to S3..."
