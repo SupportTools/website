89a071d817c834a2bb77d89fd6971adbe8afc045
Matthew Mattox <mmattox@support.tools>
2023-01-20 14:30:50 -0600

    Switrching To Docker Hub

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -3,29 +3,29 @@
 if [ ! -z "${TAG}" ]
 then
   echo "TAG is set to ${TAG}"
-  docker pull harbor.support.tools/supporttools/website:${TAG}
-  docker tag harbor.support.tools/supporttools/website:${TAG} harbor.support.tools/supporttools/website:${DRONE_BUILD_NUMBER}
-  docker push harbor.support.tools/supporttools/website:${DRONE_BUILD_NUMBER}
+  docker pull supporttools/website:${TAG}
+  docker tag supporttools/website:${TAG} supporttools/website:${DRONE_BUILD_NUMBER}
+  docker push supporttools/website:${DRONE_BUILD_NUMBER}
 else
   echo "Building image"
-  #docker pull harbor.support.tools/supporttools/website:latest
-  if ! docker build -t harbor.support.tools/supporttools/website:${DRONE_BUILD_NUMBER} --cache-from harbor.support.tools/supporttools/website:latest -f Dockerfile .
+  #docker pull supporttools/website:latest
+  if ! docker build -t supporttools/website:${DRONE_BUILD_NUMBER} --cache-from supporttools/website:latest -f Dockerfile .
   then
     echo "Problem building latest image"
     exit 1
   fi
   echo "Pushing tagged image"
-  if ! docker push harbor.support.tools/supporttools/website:${DRONE_BUILD_NUMBER}
+  if ! docker push supporttools/website:${DRONE_BUILD_NUMBER}
   then
     echo "Docker push tagged failed"
     exit 1
   fi
-  if ! docker tag harbor.support.tools/supporttools/website:${DRONE_BUILD_NUMBER} harbor.support.tools/supporttools/website:latest
+  if ! docker tag supporttools/website:${DRONE_BUILD_NUMBER} supporttools/website:latest
   then
     echo "Problem tagging latest image"
     exit 1
   fi
-  if ! docker push harbor.support.tools/supporttools/website:latest
+  if ! docker push supporttools/website:latest
   then
     echo "Docker pushing latest failed"
     exit 1
