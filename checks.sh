6d900046e765a26a67dea7e73ebe5fcce8bb975f
Matthew Mattox <mmattox@support.tools>
2023-02-02 20:32:58 -0600

    Moving checks to it's own script

---
+++ checks.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Checking for draft content"
+cd blog/content/post
+if [[ -z $(grep -R 'draft: true' . | grep -v '_template.md') ]]
+then
+  echo "No draft content found"
+else
+  echo "Draft content found"
+  exit 1
+fi
