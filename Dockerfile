483f12209588600466469071d23800e6e72f93c7
Matthew Mattox <mmattox@support.tools>
2024-02-21 00:48:06 -0600

    Switching to offical image

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,8 @@
-FROM supporttools/kube-builder:latest AS builder
-
-WORKDIR /root
-COPY hugo_*.tar.gz /root/hugo.tar.gz
-RUN tar xvzf hugo.tar.gz && \
-    cp hugo /usr/bin/hugo && \
-    chmod +x /usr/bin/hugo && \
-    rm -rf hugo.tar.gz
+FROM hugomods/hugo AS builder
 
 COPY ./blog/ /site
 WORKDIR /site
 RUN hugo
-
 
 FROM openbridge/nginx:latest
 COPY --from=builder /site/public /usr/share/nginx/html
--- deploy.sh
+++ deploy.sh
@@ -87,7 +87,7 @@ echo "HPA: ${hpa}"
 
 echo "Installing kubectl"
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
 echo "Installing helm"
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
